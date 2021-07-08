5578e48e6a2dc2a0e48bb9482bbb80413efa0685
ArturYeg <yeggart1988@gmail.com>
2021-07-08 23:33:15 +0400

    commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import './Button.css';
 
 function Button(props) {
+    const [click, setClick]=useState()
+
     return  <button 
                 type="button" 
                 className="Button" 
                 disabled={props.disabled}
+                onclick={()=>setClick(this.disabled)}
             >
                 {props.text}
             </button>
